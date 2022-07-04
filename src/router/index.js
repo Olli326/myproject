b095114d8c03d2bf151d49ad9993aa7e0ffb863a
Филиппова Ольга <o.filippova@movavi.com>
2022-07-04 20:50:27 +0700

    test webpack-cli

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,21 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import Home from '../pages/Home';
-import NotFound from './../pages/NotFound';
+import Home from '../pages/Home.vue';
 
 Vue.use(VueRouter);
 
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    }
+]
+
 const router = new VueRouter({
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home,
-        },
-        {
-            path: '*',
-            name: '404',
-            component: NotFound,
-        },
-    ]
-});
+    mode: 'history',
+    routes
+})
 
 export default router;
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,59 @@
+const path = require("path");
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-        new VueLoaderPlugin()
-    ]
-}
+const isProduction = process.env.NODE_ENV === "production";
+
+const stylesHandler = isProduction
+  ? MiniCssExtractPlugin.loader
+  : "style-loader";
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+    new VueLoaderPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, "css-loader", "sass-loader"],
+      }
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+
+    config.plugins.push(new MiniCssExtractPlugin());
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
